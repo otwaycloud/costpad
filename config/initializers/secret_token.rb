3506bd328e708a11b7eaa6afbea77c5245fe5c19
Greg McKay <greg@otwaycloud.net>
2012-12-13 08:42:37 +1100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Costpad::Application.config.secret_token = 'c159b9b268c095daad96789d53b2810fdc8d303cdbec9246e54a73678367cfa9f397de33f154d3f3cd3273f8b6430050d2b11efaed3ad4f85c616e9baf49cde2'
