d94379f26b35f1927af03221a8bb0cd02bdf9fe3
Greg McKay <greg@otwaycloud.net>
2012-12-16 10:28:08 +1100

    layouts routes user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Costpad::Application.routes.draw do
-  get "pages/home"
 
-  get "pages/about"
+ get "users/new"
+
+ root :to => 'pages#home'
+
+ match '/signup', to: 'users#new'
+
+ match '/home', to: 'pages#home'
+
+ match '/about', to: 'pages#about'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -52,7 +59,6 @@ Costpad::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-   root :to => 'pages#home'
 
   # See how all your routes lay out with "rake routes"
 
